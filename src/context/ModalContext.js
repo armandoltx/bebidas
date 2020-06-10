8e51c20aafd76234a75cf34a8a9f43543c0d4156
Armando Ruiz <armando.ruiz@industrie.co>
2020-06-10 12:02:57 +1000

    181 Conslutando la API para obtener la receta y guardarla en el state

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -10,6 +10,26 @@ const ModalProvider = (props) => {
   // Creamos un state del provider, lo q nos interesa es guardar el id al q el usuario de click
   const [ idreceta, guardarIdReceta ] = useState(null); // no va a ver ninguna seleccionada
 
+  // creamos otro state para guardar la receta que viene de la API en resultado
+  const [ receta, guardarReceta ] = useState({});
+
+  // una vez que tenemos la id de la receta gracias a guardarIdReceta queremos hacer
+  // una llamada a la API
+  useEffect( () => {
+    const obtenerReceta = async () => {
+      if(!idreceta) return;
+
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+      const resultado = await axios.get(url);
+      // console.log(resultado.data.drinks[0]);
+      guardarReceta(resultado.data.drinks[0]);
+
+    }
+    obtenerReceta();
+
+  },[idreceta] );
+
 
 
 
